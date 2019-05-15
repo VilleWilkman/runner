e01a075c34bb4cdb60961dc825e0c0add7936558
Ville Wilkman <ville.wilkman@myy.haaga-helia.fi>
2019-05-15 22:21:05 +0300

    Runner folder and script

---
+++ runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Creating folder and cloning Git -repositories and running setuplinux.sh script..."
+
+sudo mkdir -p /srv/salt
+
+cd /srv/salt
+
+sudo git clone https://github.com/VilleWilkman/moduuli.git
+
+cd moduuli
+
+sudo chmod +rx setuplinux.sh
+
+echo "Now it's starting..."
+
+./setuplinux.sh
+
+
